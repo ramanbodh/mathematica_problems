5d6c26c85e441c95561c70e94b41706b028d690d
ramanbodh <crazybodh14@gmail.com>
2026-09-05 19:37:21 +0530

    it think we are somewhere near compleating it

--- determinantOfNxNmatrix.py
+++ determinantOfNxNmatrix.py
@@ -1,22 +1,32 @@
 #finding the determinant of n x n  matrices
 
-def determinant(arrayX,sum=0,mul=1):
+def determinant(arrayX):
     if len(arrayX[0])==2:
-        sum=((arrayX[0][0]*arrayX[1][1])-(arrayX[1][0]*arrayX[0][1]))*mul
+        sum=((arrayX[0][0]*arrayX[1][1])-(arrayX[1][0]*arrayX[0][1]))
         return sum
     newarry=[]
     mul=0
-    for i in len(arrayX):
-        for j in len(arrayX):
-            if i==j:
-                mul=arrayX[i][j]
-                continue
-            if j==0:
-                continue
-            if 
-            newarry=arrayX[i][j]
-    sum+=determinant(arrayX,sum,mul)
-    retun sum
+    result=0
+    for i in range(len(arrayX)):
+        q=0
+
+        mul=arrayX[i][0]
+        for t in range(len(arrayX)):
+            p=0
+            for j in range(len(arrayX)):
+                if j==0 or i==t:
+                     continue 
+                newarry[q][p]=arrayX[t][j]
+                p+=1
+            q+1
+        result+=(determinant(newarry))*mul
+
+    return result
+
+arraya=[[1,3,2],[-3,-1,-3],[2,3,1]]
+result=determinant(arraya)
+print(result)
+        
             
                 
             
